88b8cc0ba60da5bc0fcca9aefb3dbbc06ac3d88d
joker-jonesy <spashmcgrath94@yahoo.com>
2022-08-30 10:00:36 -0400

    Kermit

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,8 +1,13 @@
 import React from "react";
-
 import {Link, NavLink} from "react-router-dom";
 
 function Nav(){
+
+    const [toggle, setToggle]=React.useState(true);
+
+    let style= {
+        right: (toggle ? "-100%":"0")
+    }
 
     return(
         <nav className={"dark"}>
@@ -12,6 +17,14 @@ function Nav(){
                 <NavLink to={"/"} activeclassname={"active"}>Home</NavLink>
                 <NavLink to={"/list"} activeclassname={"active"}>List</NavLink>
                 <NavLink to={"/admin"} activeclassname={"active"}>Admin</NavLink>
+            </div>
+            <div className="drop-down" onClick={()=>setToggle(!toggle)}>
+                Drop
+            </div>
+            <div className="side-bar" style={style}>
+                <Link to={"/"} >Home</Link>
+                <Link to={"/list"}>List</Link>
+                <Link to={"/admin"} >Admin</Link>
             </div>
 
 
