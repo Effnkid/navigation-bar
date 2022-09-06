36082cc5e47810a9415a9f87f01194f966f73001
Effnkid <l.bobbsemple.gmail.com>
2022-09-06 10:53:28 -0400

    gum 1

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,22 +1,42 @@
-import React from "react";
+import React from 'react';
 
-import {Link, NavLink} from "react-router-dom";
+import { Link, NavLink } from 'react-router-dom';
 
-function Nav(){
+function Nav() {
+	const [toggle, setToggle] = React.useState(true);
 
-    return(
-        <nav className={"dark"}>
-            <Link className={"logo"} to={"/"}>Logo</Link>
+	let style = {
+		right: toggle ? '-100%' : '0',
+	};
 
-            <div className="link-wrapper">
-                <NavLink to={"/"} activeclassname={"active"}>Home</NavLink>
-                <NavLink to={"/list"} activeclassname={"active"}>List</NavLink>
-                <NavLink to={"/admin"} activeclassname={"active"}>Admin</NavLink>
-            </div>
+	return (
+		<nav className={'dark'}>
+			<Link className={'logo'} to={'/'}>
+				Logo
+			</Link>
 
-
-        </nav>
-    )
+			<div className="link-wrapper">
+				<NavLink to={'/'} activeclassname={'active'}>
+					Home
+				</NavLink>
+				<NavLink to={'/list'} activeclassname={'active'}>
+					List
+				</NavLink>
+				<NavLink to={'/admin'} activeclassname={'active'}>
+					Admin
+				</NavLink>
+			</div>
+			<div className="drop-down" onClick={() => setToggle(!toggle)}>
+				{' '}
+				Drop{' '}
+			</div>
+			<div className="side-bar" style={style}>
+				<Link to={'/'}>Home</Link>
+				<Link to={'/list'}>List</Link>
+				<Link to={'/admin'}>Admin</Link>
+			</div>
+		</nav>
+	);
 }
 
 export default Nav;
